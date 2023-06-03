709814b3e5508492f62ad442c8799526a722a687
Erik Neves <erikrrn04@gmail.com>
2023-06-02 21:08:59 -0300

    prova 1 main

--- Provas/Prova-01/src/main.cpp
+++ Provas/Prova-01/src/main.cpp
@@ -3,8 +3,7 @@
 
 #include "../include/sistema.h"
 
-int main() 
-{
+int main() {
     Sistema sistema_uber = Sistema();
 
     sistema_uber.adicionar_carro("branco", 0);
@@ -12,45 +11,48 @@ int main()
     sistema_uber.adicionar_carro("verde", 1);
     sistema_uber.adicionar_carro("vermelho", 10);
 
+    try {
+        sistema_uber.adicionar_carro("verde", 1);
+    } catch (id_ja_existe_e &e) {
+        std::cout << "Id do carro já existe!" << std::endl;
+    }
+
     Cliente* flavio = sistema_uber.cadastra_cliente("flavio@dcc");
     Cliente* ana = sistema_uber.cadastra_cliente("ana@dcc");
     Cliente* pedro = sistema_uber.cadastra_cliente("pedro@dcc");
 
-    try 
-    {
+    try {
         sistema_uber.cadastra_cliente("flavio@dcc");
-    } catch (cliente_ja_existe_e &e) 
-    {
+    } catch (cliente_ja_existe_e &e) {
         std::cout << "O cliente já existe!" << std::endl;
     }
 
-    try 
-    {
+    try {
         flavio->adicionar_saldo(-2);
-    } catch (impossivel_adicionar_saldo_negativo_e &e) 
-    {
+    } catch (impossivel_adicionar_saldo_negativo_e &e) {
         std::cout << "Não podemos adicionar saldo negativo" << std::endl;
     }
 
     flavio->adicionar_saldo(50);
-    ana->adicionar_saldo(30);
-    pedro->adicionar_saldo(30);
+    ana->adicionar_saldo(20);
 
-    try 
-    {
+    try {
         ana->remover_saldo(50);
-    } catch (saldo_insuficiente_para_remocao_e &e) 
-    {
+    } catch (saldo_insuficiente_para_remocao_e &e) {
         std::cout << "Não posso remover saldo de valor 50 de ana" << std::endl;
     }
 
-    Carro * cf = sistema_uber.busca_uber("flavio@dcc");
-    std::cout << "Carro de placa " << cf->get_id();
-    std::cout << " está ocupado? " << cf->ocupado() << std::endl;
+    Carro* carro_flavio = sistema_uber.busca_uber("flavio@dcc");
+    std::cout << "Carro de placa " << carro_flavio->get_id();
+    std::cout << " está ocupado? " << carro_flavio->ocupado() << std::endl;
     
-    Carro * ca = sistema_uber.busca_uber("ana@dcc");
-    std::cout << "Carro de placa " << ca->get_id();
-    std::cout << " está ocupado? " << ca->ocupado() << std::endl;
+    Carro* carro_ana = sistema_uber.busca_uber("ana@dcc");
+    std::cout << "Carro de placa " << carro_ana->get_id();
+    std::cout << " está ocupado? " << carro_ana->ocupado() << std::endl;
     
-    sistema_uber.busca_uber("pedro@dcc");
+    try {
+        sistema_uber.busca_uber("pedro@dcc");
+    } catch (sem_saldo_e &e) {
+        std::cout << "Não posso alocar um carro para pedro!" << std::endl;
+    }
 }
